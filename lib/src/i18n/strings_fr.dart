15a2c7cecef2eb5c2cf1a4b135ae424c95e5f941
bface007 <bface007@gmail.com>
2020-01-12 23:47:54 +0000

    add short months names

--- lib/src/i18n/strings_fr.dart
+++ lib/src/i18n/strings_fr.dart
@@ -33,6 +33,24 @@ class _StringsFr extends _StringsI18n {
   }
 
   @override
+  List<String> getMonthsShort() {
+    return [
+      "Janv.",
+      "Févr.",
+      "Mars",
+      "Avr.",
+      "Mai",
+      "Juin",
+      "Juil.",
+      "Août",
+      "Sept.",
+      "Oct.",
+      "Nov.",
+      "Déc."
+    ];
+  }
+
+  @override
   List<String> getWeeksFull() {
     return [
       "Lundi",
@@ -56,11 +74,5 @@ class _StringsFr extends _StringsI18n {
       "Sam.",
       "Dim.",
     ];
-  }
-
-  @override
-  List<String> getMonthsShort() {
-    // TODO: implement getMonthsShort
-    return null;
   }
 }
