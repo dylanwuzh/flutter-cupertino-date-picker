f2cb74c6f07340507c2499fa9140d42a08aecf17
Joshua de Guzman <joshua.maiquez.deguzman@gmail.com>
2021-03-04 11:26:52 +0800

    Remove deprecated shadow theme

--- lib/src/date_picker.dart
+++ lib/src/date_picker.dart
@@ -100,7 +100,7 @@ class DatePicker {
         onCancel: onCancel,
         onChange: onChange,
         onConfirm: onConfirm,
-        theme: Theme.of(context, shadowThemeOnly: true),
+        theme: Theme.of(context),
         barrierLabel:
             MaterialLocalizations.of(context).modalBarrierDismissLabel,
         minuteDivider: minuteDivider,
